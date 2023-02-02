73d3bba5e2bb6b5f87c07c39a5d2de97c5b1298c
Amin Mangeot <mamindegs@hotmail.fr>
2023-02-02 10:55:49 +0100

    custom chat front

--- app/src/components/MyMessage.js
+++ app/src/components/MyMessage.js
@@ -4,15 +4,15 @@ import Moment from "react-moment";
 // Component to represent a single message from current user
 const MyMessage = ({ message, name, date, avatar }) => {
   return (
-    <div className="flex flex-row gap-2 justify-end my-2 mx-1">
-      <div className="flex flex-col">
+    <div className="flex flex-1 flex-row gap-2 justify-end my-2 mx-1">
+      <div className="flex flex-col bg-[#0560FD] text-white w-25 rounded-lg rounded-r-none p-2">
         <div className="flex flex-row gap-2">
           <div className="font-bold">{name}</div>
           <div className="text-[#BDBDBD]">
             <Moment format="hh:mm A">{date}</Moment>
           </div>
         </div>
-        <div className="text-[#212325]">{message}</div>
+        <p className="text-white">{message}</p>
       </div>
       <img src={avatar} className="w-9 h-9 bg-[#aaa] rounded-full cursor-pointer object-cover" />
     </div>
--- app/src/scenes/chat/index.js
+++ app/src/scenes/chat/index.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import api from "../../services/api";
-import Moment from "react-moment";
 import MyMessage from "../../components/MyMessage";
 import Message from "../../components/Message";
+import Moment from "react-moment";
 const Chat = () => {
   const user = useSelector((state) => state.Auth.user);
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
   const messagesList = useRef(null);
+  let currentDate = new Date("August 19, 1975 23:15:30").toDateString();
 
   useEffect(() => {
     // Fetch messages from server and scroll to bottom of messages list on load and every 60 seconds
@@ -24,8 +25,8 @@ const Chat = () => {
     return () => clearInterval(interval);
   }, []);
 
+  // Send message to server
   const handleSubmit = async (event) => {
-    // Send message to server
     event.preventDefault();
     const { data, ok } = await api.post(`/chat/new`, { content: newMessage });
     ok && setMessages([...messages, data]);
@@ -35,16 +36,40 @@ const Chat = () => {
     setNewMessage("");
   };
 
+  // Render message based on if it's from the current user or not
+  function RenderMessage(message, index) {
+    return (
+      <div key={index}>
+        {message.name === user.name ? (
+          <MyMessage message={message.content} date={message.date} name={message.name} avatar={message.avatar} />
+        ) : (
+          <Message message={message.content} date={message.date} name={message.name} avatar={message.avatar} />
+        )}
+      </div>
+    );
+  }
+
   return (
-    <div className="px-2 md:!px-8 flex flex-col md:fex-column gap-5 mt-5 h-50">
+    <div className="px-2 md:!px-8 flex flex-col md:fex-column gap-5 mt-5 h-90">
       <ul ref={messagesList} className="flex-1 border-8 border-r-8 bg-white shadow-md overflow-y-auto m-0 p-0 list-none flex flex-col h-64">
-        {messages?.map((message, index) => {
-          if (message.name === user.name) {
-            return <MyMessage key={index} message={message.content} date={message.date} name={message.name} avatar={message.avatar} />;
-          } else {
-            return <Message key={index} message={message.content} date={message.date} name={message.name} avatar={message.avatar} />;
-          }
-        })}
+        <ul ref={messagesList} className="flex-1 border-8 border-r-8 bg-white shadow-md overflow-y-auto m-0 p-0 list-none flex flex-col h-64">
+          {messages?.map((message, index) => {
+            let dateMessage = new Date(message.date).toDateString();
+            if (dateMessage !== currentDate) {
+              currentDate = new Date(message.date).toDateString();
+              return (
+                <>
+                  <p className="text-center text-gray-600">
+                    <Moment format="MMM Do YY">{currentDate}</Moment>
+                  </p>
+                  {RenderMessage(message, index)}
+                </>
+              );
+            } else {
+              return RenderMessage(message, index);
+            }
+          })}
+        </ul>
       </ul>
       <form onSubmit={handleSubmit} className="flex gap-2">
         <input
